a3d975b8229f738d5d1874f7b63c6eec419f8dd6
Burenkovuser <nb2207@yandex.ru>
2019-08-15 09:44:11 +0700

    Initial Commit

---
+++ Table cells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table cells
+//
+//  Created by Vasilii on 15/08/2019.
+//  Copyright © 2019 Vasilii Burenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
